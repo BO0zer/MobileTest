9ecda775a7097ce8f4bd5126b306b41cec44716a
BO0zer <yurgroshev@edu.hse.ru>
2021-10-08 12:26:14 +0500

    1 вкладка

---
+++ MobileTest/MobileTest/CameraData.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Net;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MobileTest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CameraData : ContentPage
+    {
+        public List<Camera> Cameras { get; set; } = new List<Camera>();
+        public CameraData(CfgHolder cfgHolder)
+        {
+            InitializeComponent();
+            Cameras = cfgHolder.GetChannels();
+            BindingContext = this;
+        }
+
+    }
+}
