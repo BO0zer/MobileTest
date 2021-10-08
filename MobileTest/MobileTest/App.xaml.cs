69da1ae7cec9da362de2a1c1ab8ab4fbff5cc5ce
BO0zer <yurgroshev@edu.hse.ru>
2021-10-08 12:27:30 +0500

    Изменен запуск приложения (запуск конфига)

--- MobileTest/MobileTest/App.xaml.cs
+++ MobileTest/MobileTest/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,9 +9,21 @@ namespace MobileTest
     {
         public App()
         {
+            const string url = "http://demo.macroscop.com:8080/configex?login=root";
+
             InitializeComponent();
 
-            MainPage = new MainPage();
+            CfgHolder cfgHolder = new CfgHolder(XDocument.Load(url));
+            var pageCameraData = new CameraData(cfgHolder);
+            var pageCameraFrames = new CameraFrames(cfgHolder);
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    pageCameraData,
+                    pageCameraFrames
+                }
+            };
         }
 
         protected override void OnStart()
