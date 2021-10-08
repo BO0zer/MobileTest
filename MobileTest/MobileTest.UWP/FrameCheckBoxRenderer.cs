f23773ca76f382afc905d2186e6609f70f8aeb5c
BO0zer <yurgroshev@edu.hse.ru>
2021-10-08 12:47:34 +0500

    Рендер FrameCheckBox

---
+++ MobileTest/MobileTest.UWP/FrameCheckBoxRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Xamarin.Forms.Platform.UWP;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using MobileTest;
+using MobileTest.UWP;
+
+[assembly: ExportRenderer(typeof(FrameCheckBox), typeof(FrameCheckBoxRenderer))]
+namespace MobileTest.UWP
+{
+    class FrameCheckBoxRenderer: CheckBoxRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.CheckBox> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                //Control.Background = new SolidColorBrush(Windows.UI.Colors.LightBlue);
+                //Control.BorderBrush = new SolidColorBrush(Windows.UI.Colors.CadetBlue);
+            }
+        }
+    }
+}
