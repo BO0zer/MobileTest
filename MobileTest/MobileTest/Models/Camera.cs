5a2037d305be45821d10fc24bc4c34e1f4818141
BO0zer <yurgroshev@edu.hse.ru>
2021-10-08 12:24:34 +0500

    Создана модель камер

---
+++ MobileTest/MobileTest/Models/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileTest
+{
+    public class Camera
+    {
+        public Image Frame { get; set; }
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool DisplayFrame { get; set; }
+        public bool IsSoundOn { get; set; }
+
+        public string SecObjectInfoName { get; set; }
+
+        public Camera(string name, bool isSoundOn, string secObjectInfoName, Guid id)
+        {
+            Id = id;
+            Name = name;
+            IsSoundOn = isSoundOn;
+            SecObjectInfoName = secObjectInfoName;
+            Frame = new Image();
+        }
+    }
+}
